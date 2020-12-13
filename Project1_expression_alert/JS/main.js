fae55ec36b6d50c9c4f4fdfb6265b110d255281b
Cindy <cindy123jl@gmail.com>
2020-12-12 22:00:28 -0800

    initial-commit

--- Project1_expression_alert/JS/main.js
+++
@@ -1,9 +0,0 @@
-window.alert("Hiya");//here I am writing an alert pop up that says hiya
-document.write('Welcome!! Enjoy!<br>');//here I am outputting Welcome!! Enjoy! on the screen
-var b ="I hope your day is wonderful!<br><br>"//here I am assigning the string "I hope your day is wonderful!" a value of b
-var b=b.fontcolor("orange");//here I am telling the variable b string to have a font color of orange
-document.write(b);//here I am calling the variable b to display its string
-document.write("The sun asked,\"Why so sad moon?\"<br>\"No one seems to appreciate me, when I come out everyone hides inside :(\"The moon said.<br><br>");
-//here I am adding a diologue onto the webpage
-document.write("\"You are special in your own way,"+"You are appreciated.\""+"-The Sun");
-//her I am addiing a concatenated string
