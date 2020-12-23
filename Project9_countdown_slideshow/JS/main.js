4f85061f9fe9d7af33913d40ce0e3012536adf11
Cindy <cindy123jl@gmail.com>
2020-12-22 20:50:25 -0800

    Update main.js

--- Project9_countdown_slideshow/JS/main.js
+++ Project9_countdown_slideshow/JS/main.js
@@ -1,8 +1,8 @@
-var slideIndex=1;
+var slideIndex=1;// variable slideIndex is assigned a value of 1
 showSlides(slideIndex);
 
 //next/previous controls
-function plusSlides(n){
+function plusSlides(n){ //plusSlides has a parameter n
     showSlides(slideIndex +=n);
 }
 
@@ -12,15 +12,15 @@ function currentSlide(n){
 }
 
 function showSlides(n){
-    var i;
-    var slides=document.getElementsByClassName("mySlides");
-    var dots=document.getElementsByClassName("dot");
-    if(n>slides.length){slideIndex=1}
-    if(n<1){slideIndex=slides.length}
-    for(i=0; i<slides.length; i++){
-        slides[i].style.display="none";
+    var i; //variable is i
+    var slides=document.getElementsByClassName("mySlides");//variable slides is retrieving elements of mySlides
+    var dots=document.getElementsByClassName("dot"); //variable dots is retrieving elements of dot
+    if(n>slides.length){slideIndex=1}//if slides length is less than n
+    if(n<1){slideIndex=slides.length}//if n is less than 1 than the slide index = slides.length
+    for(i=0; i<slides.length; i++){//for i = 0 i is less than the slides length increment by one
+        slides[i].style.display="none";//display none
     }
-    for(i=0; i<dots.length;i++){
+    for(i=0; i<dots.length;i++){// for i = 0 i is less than dots increment by 1
         dots[i].className=dots[i].className.replace(" active", "");
     }
     slides[slideIndex-1].style.display= "block";
