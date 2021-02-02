3be8d79cdaa9218adc187382c50f7baaaf9a95b2
Cindy <cindy123jl@gmail.com>
2021-02-02 15:51:50 -0800

    initial-commit

---
+++ JS/main.js
@@ -0,0 +1,13 @@
+window.alert("Hiya");//here I am writing an alert pop up that says hiya
+document.write('Welcome!! Enjoy!<br>');//here I am outputting Welcome!! Enjoy! on the screen
+var b ="I hope your day is wonderful!"//here I am assigning the string "I hope your day is wonderful!" a value of b
+var b=b.fontcolor("orange");//here I am telling the variable b string to have a font color of orange
+var c =" What is your name?<br><br>"//created another variable
+var d = b + c;//here I concated variable b and c
+document.write(d);//This displays both strings
+document.write("The sun asked,\"Why so sad moon?\"<br>\"No one seems to appreciate me, when I come out everyone hides inside :(\"The moon said.<br><br>");
+//here I am adding a diologue onto the webpage
+document.write("\"You are special in your own way,"+"You are appreciated.\""+"-The Sun<br><br>"); //her I am adding a concatenated string
+document. write(" 56 + 33 =  ");//printing this statement out to go along with our expression
+document.write(56+33);//This is an expressions 56 +  33
+
