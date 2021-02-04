e25e79b70d85303ff38c74e2da19778ddb1b7bd9
Cindy <cindy123jl@gmail.com>
2021-02-03 21:49:37 -0800

    initial-commit

--- Project9_countdown_slideshow/JS/main.js
+++ Project9_countdown_slideshow/JS/main.js
@@ -1,28 +1,14 @@
-var slideIndex=1;// variable slideIndex is assigned a value of 1
-showSlides(slideIndex);
+var myIndex = 0;
+countdown();
 
-//next/previous controls
-function plusSlides(n){ //plusSlides has a parameter n
-    showSlides(slideIndex +=n);
-}
-
-//thumbnail image controls
-function currentSlide(n){
-    showSlides(slideIndex=n);
-}
-
-function showSlides(n){
-    var i; //variable is i
-    var slides=document.getElementsByClassName("mySlides");//variable slides is retrieving elements of mySlides
-    var dots=document.getElementsByClassName("dot"); //variable dots is retrieving elements of dot
-    if(n>slides.length){slideIndex=1}//if slides length is less than n
-    if(n<1){slideIndex=slides.length}//if n is less than 1 than the slide index = slides.length
-    for(i=0; i<slides.length; i++){//for i = 0 i is less than the slides length increment by one
-        slides[i].style.display="none";//display none
-    }
-    for(i=0; i<dots.length;i++){// for i = 0 i is less than dots increment by 1
-        dots[i].className=dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display= "block";
-    dots[slideIndex-1].className+=" active";
+function countdown() { //countdown function 
+  var i;//our vairable i
+  var x = document.getElementsByClassName("mySlides");//retrieving by 
+  for (i = 0; i < x.length; i++) {//incrementing 1 for i
+    x[i].style.display = "none";  //this allows for the slideshow to dislplay properly one image at a time
+  }
+  myIndex++;//incrementing by 1
+  if (myIndex > x.length) {myIndex = 1}  //allows images to go in a loop 
+  x[myIndex-1].style.display = "block";  //allows for our images display
+  setTimeout(countdown, 3500); // changes image every 3.5 seconds
 }
